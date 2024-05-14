410d6f929705fd249803f4b6c0a0881cf0d676c1
Hachimankira <kiran9860819025@gmail.com>
2024-05-14 20:22:04 +0545

    footer

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const Footer = () => {
   return (
-    <div className={styles}>Footer</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.png" alt="logo" width={50} height={50} />
+          <h1 className={styles.logoText}>HachiBlog</h1>
+        </div>
+        <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur ullam iusto consectetur aut exercitationem dolor.</p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="facebook" width={18} height={18} />
+          <Image src="/instagram.png" alt="instagram" width={18} height={18} />
+          <Image src="/tiktok.png" alt="tiktok" width={18} height={18} />
+          <Image src="/youtube.png" alt="youtube" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+        </div>
+
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Style</Link>
+          <Link href="/">Coding</Link>
+          <Link href="/">Fashion</Link>
+          <Link href="/">Travel</Link>
+        </div>
+
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Socail</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+
+    </div>
+  );
 }
