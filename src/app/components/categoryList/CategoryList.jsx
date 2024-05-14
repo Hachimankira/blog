f3bbe5a792103bc953db73ef9b8cd71cca4afa8b
Hachimankira <kiran9860819025@gmail.com>
2024-05-14 17:31:01 +0545

    categories section made

--- src/app/components/categoryList/CategoryList.jsx
+++ src/app/components/categoryList/CategoryList.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
 import styles from './categoryList.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const CategoryList = () => {
   return (
-    <div className={styles.container}>CategoryList</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+          <Image src="/style.png" alt='img' width={32} height={32} className={styles.image} />
+          style
+        </Link>
+        <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+          <Image src="/fashion.png" alt='img' width={32} height={32} className={styles.image} />
+          fashion
+        </Link>
+        <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+          <Image src="/food.png" alt='img' width={32} height={32} className={styles.image} />
+          food
+        </Link>
+        <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+          <Image src="/travel.png" alt='img' width={32} height={32} className={styles.image} />
+          travel
+        </Link>
+        <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+          <Image src="/culture.png" alt='img' width={32} height={32} className={styles.image} />
+          culture
+        </Link>
+        <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+          <Image src="/coding.png" alt='img' width={32} height={32} className={styles.image} />
+          coding
+        </Link>
+
+      </div>
+    </div>
   )
 }
--- src/app/components/featured/Featured.jsx
+++ src/app/components/featured/Featured.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import styles from './featured.module.css'
+import Image from 'next/image'
 
 export const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Kiran Shrestha here!</b> Discover my latest blogs and articles.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" alt="img" fill className={styles.image} />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <p className={styles.postDesc}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta perferendis a maiores, porro inventore voluptatibus molestias quas corrupti doloribus illo! Corporis quos repellendus fuga qui autem! Repellat, cumque doloremque! Aliquid?</p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   )
 }
