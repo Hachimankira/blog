15a796db086096d1fd98222e5e1b85fdf57ac7f8
Hachimankira <kiran9860819025@gmail.com>
2024-05-14 15:00:25 +0545

    toggle betwn dark and light mode

---
+++ src/app/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './navbar.module.css'
+import ThemeToggle from '../themeToggle/ThemeToggle'
+import AuthLinks from '../authLinks/AuthLinks'
+import Image from 'next/image'
+
+export const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.social}>
+        <Image src="/facebook.png" alt="facebook" width={24} height={24} />
+        <Image src="/instagram.png" alt="facebook" width={24} height={24} />
+        <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
+        <Image src="/youtube.png" alt="youtube" width={24} height={24} />
+      </div>
+      <div className={styles.logo}>HachiBlog</div>
+      <div className={styles.links}>
+        <ThemeToggle />
+        <a href="/" >Homepage</a>
+        <a href="/contact" >Contact</a>
+        <a href="/about" >About</a>
+        <AuthLinks />
+      </div>
+    </div>
+  )
+}
