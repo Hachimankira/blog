263cb8561b85d0882145543901cc072b60bddbe1
Hachimankira <kiran9860819025@gmail.com>
2024-05-15 22:55:05 +0545

    some changes

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -2,16 +2,17 @@
 import Image from 'next/image';
 import styles from './write.module.css'
 import { useEffect, useState } from 'react';
-import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { app } from '@/utils/firebase';
+import dynamic from 'next/dynamic';
 
 const storage = getStorage(app);
 
 const WritePage = () => {
+    const ReactQuill = dynamic(() => import('react-quill'), { ssr: false });
 
     const { data, status } = useSession();
     const router = useRouter();
