da1b7551943b6d06c4deffa8e4a79e12e6fa8702
Hachimankira <kiran9860819025@gmail.com>
2024-05-15 12:39:23 +0545

    authentication using OAuth

---
+++ src/utils/auth.js
@@ -0,0 +1,16 @@
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET ,
+        }),
+    ],
+    // providers: [GitHub]
+};
