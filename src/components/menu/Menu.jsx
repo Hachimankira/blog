006c065ac34b941cc2b59fec5f312a7f85c7c35f
Hachimankira <kiran9860819025@gmail.com>
2024-05-17 13:17:19 +0545

    editor choice completed

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -5,7 +5,19 @@ import Link from 'next/link'
 import { MenuPosts } from '../menuPosts/MenuPosts'
 import { MenuCategories } from '../menuCategories/MenuCategories'
 
-export const Menu = () => {
+const getData = async () => {
+  const res = await fetch(`http://localhost:3000/api/editor`, {
+      cache: 'no-store',
+  });
+
+  // if (!res.ok) {
+  //   throw new Error("Failed");
+  // }
+  return res.json();
+}
+
+export const Menu = async () => {
+  const editorChoice =await getData();
   return (
     <div className={styles.container}>
       <h2 className={styles.subtitle}>What&apos;s hot</h2>
@@ -19,7 +31,7 @@ export const Menu = () => {
 
       <h2 className={styles.subtitle}>Choosen by the editor</h2>
       <h1 className={styles.title}>Editor Pick</h1>
-      <MenuPosts withImage={true} />
+      <MenuPosts withImage={true} posts={editorChoice}/>
     </div>
   )
 }
--- src/components/menuPosts/MenuPosts.jsx
+++ src/components/menuPosts/MenuPosts.jsx
@@ -3,27 +3,36 @@ import Link from 'next/link';
 import React from 'react'
 import styles from './menuposts.module.css'
 
-export const MenuPosts = ({ withImage }) => {
-    return (
-        <div className={styles.items}>
+export const MenuPosts = ({ withImage, posts }) => {
+  if (!posts) {
+    return null; // or return some placeholder content
+  }
+
+  const datas = posts;
+  return (
+
+    <div className={styles.items}>
+      {datas.map((post) => (
         <Link href="/blog?cat=style" className={styles.item}>
-          {withImage && (
+          {withImage && post?.image && (
             <div className={styles.imageContainer}>
-            <Image src="/p1.jpeg" alt='img' fill className={styles.image}/>
-          </div>
+              <Image src={post?.image}alt='img' fill className={styles.image} />
+            </div>
           )}
-          
+
           <div className={styles.textContainer}>
             <span className={`${styles.category} ${styles.food}`}>
-              Food
+            {post?.catSlug}
             </span>
-            <h3 className={styles.postTitle}>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h3>
+            <h3 className={styles.postTitle}>{post?.title}</h3>
             <div className={styles.detail}>
-            <span className=''>John Doe</span>
-            <span className={styles.date}> - 11.03.2025</span>
+              <span className=''>{post?.user?.name}</span>
+              <span className={styles.date}> - {post?.createdAt}</span>
             </div>
           </div>
         </Link>
-      </div>
-    );
+      ))}
+    </div>
+
+  );
 }
